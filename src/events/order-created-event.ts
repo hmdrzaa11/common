ca7568a73b117853abeb8c741de2a8a59df4aa7f
Hamid Oujand <hmdrzaa11@gmail.com>
2022-02-28 15:42:11 +0330

    update common module

---
+++ src/events/order-created-event.ts
@@ -0,0 +1,13 @@
+import { OrderStatus } from "..";
+import { Subjects } from "./subjects";
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    userId: string;
+    expiresAt: string;
+    status: OrderStatus;
+    ticket: { id: string; price: number };
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,6 @@ export * from "./events/publishers/publisher";
 export * from "./events/subjects";
 export * from "./events/ticket-updated-event";
 export * from "./events/types/order-status";
+
+export * from "./events/order-cancelled-event";
+export * from "./events/order-created-event";
